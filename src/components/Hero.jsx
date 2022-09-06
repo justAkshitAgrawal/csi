eb114840dc840dd7902dbb59b5b130524ae22af9
Akshit Agrawal <71216807+justAkshitAgrawal@users.noreply.github.com>
2022-09-06 21:48:34 +0530

    few changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ import Socials from "./Socials";
 
 function Hero({ setPage }) {
   return (
-    <div className=" bg-[#2f3340] h-[80vh] w-[80vw] lg:w-[37vw] rounded-3xl shadow-xl flex flex-col items-center p-5">
-      <div className=" p-3 bg-[#3E4352] rounded-full shadow-lg mt-4">
+    <div className=" bg-[#2f3340] h-fit w-[80vw] lg:h-[70vh] lg:w-[37vw] rounded-3xl shadow-xl flex flex-col items-center p-5 space-y-4">
+      <div className=" p-3 bg-[#3E4352] rounded-full shadow-lg mt-2">
         <div className=" p-2 rounded-full bg-gradient-to-r from-[#7A61DA] to-[#ff7a7a] shadow-lg">
           <img
             src={logo}
@@ -15,14 +15,14 @@ function Hero({ setPage }) {
           />
         </div>
       </div>
-      <h1 className=" text-center text-7xl lg:text-8xl tracking-wide lg:tracking-normal mt-10 font-bold bg-gradient-to-tr from-[#7A61DA] to-[#ff7a7a] bg-clip-text text-transparent">
+      <h1 className=" text-center text-6xl lg:text-8xl tracking-wide lg:tracking-normal  font-bold bg-gradient-to-tr from-[#7A61DA] to-[#ff7a7a] bg-clip-text text-transparent">
         CSI <br className=" lg:hidden" />
         DITU
       </h1>
       <h1 className=" text-center lg:text-4xl text-2xl tracking-wide mt-1 text-[#A6A8AD] font-semibold">
         Student Chapter
       </h1>
-      <div className=" mt-16 lg:mt-24">
+      <div className=" mt-16 lg:pt-20">
         <Button title="About Us" setPage={setPage} />
       </div>
       <div className=" flex space-x-3 mt-3">
