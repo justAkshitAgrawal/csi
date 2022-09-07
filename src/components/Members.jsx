a467be2aa0d334fadfbcd181c08ef3075e0c60df
Akshit Agrawal <71216807+justAkshitAgrawal@users.noreply.github.com>
2022-09-07 19:44:50 +0530

    Members added

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -8,10 +8,10 @@ function Members({ name, position, image }) {
         alt=""
         className=" h-16 w-16 rounded-xl lg:w-28 lg:h-28 group-hover:grayscale transition-all group-hover:scale-110 cursor-pointer"
       />
-      <h1 className=" bg-black/50 p-2 rounded-xl text-center hidden group-hover:absolute group-hover:block text-sm lg:text-xl font-semibold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white cursor-pointer">
+      <h1 className=" bg-black/50 p-1 rounded-xl text-center hidden group-hover:absolute group-hover:block text-sm lg:text-xl font-semibold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white cursor-pointer">
         {name}
         <br />
-        <span className="text-sm">{position}</span>
+        <span className="text-xs lg:text-sm">{position}</span>
       </h1>
     </div>
   );
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -8,11 +8,13 @@ import mait from "../media/maitreyee.jpg";
 import ankit from "../media/ankit.jpg";
 import ambar from "../media/ambar.jpg";
 import abhishek from "../media/abhishek.jpeg";
+import van from "../media/van.jpeg";
+import amogh from "../media/amogh.jpg";
 
 function Team({ setPage }) {
   return (
     <div className=" bg-[#2f3340] h-fit w-[80vw] lg:h-fit lg:w-[37vw] rounded-3xl shadow-xl flex flex-col items-center p-10 lg:pb-9 space-y-4">
-      <div className=" self-start">
+      <div className="self-start ">
         <Button title="back" setPage={setPage} />
       </div>
       <h1 className=" text-center text-4xl lg:text-6xl self-start mt-10 font-bold bg-gradient-to-tr from-[#7A61DA] to-[#ff7a7a] bg-clip-text text-transparent">
@@ -21,17 +23,19 @@ function Team({ setPage }) {
       <h1 className="self-start text-center lg:text-3xl text-lg tracking-wide mt-1 text-[#A6A8AD] font-semibold pt-2">
         IV Year Team
       </h1>
-      <div className=" grid grid-cols-3 self-start gap-3 lg:gap-5 lg:grid-cols-5">
+      <div className="grid self-start grid-cols-3 gap-3 lg:gap-5 lg:grid-cols-5">
         <Members name="Abhishek" position="President" image={abhishek} />
       </div>
       <h1 className="self-start text-center lg:text-3xl text-lg tracking-wide mt-1 text-[#A6A8AD] font-semibold pt-2">
         III Year Team
       </h1>
-      <div className=" grid grid-cols-3 self-start gap-3 lg:gap-5 lg:grid-cols-5">
+      <div className="grid self-start grid-cols-3 gap-3 lg:gap-5 lg:grid-cols-5">
         <Members name="Akshit" position="Club Head" image={akshit} />
+        <Members name="Vanshika" position="Secretary" image={van} />
         <Members name="Yashdeep" position="Tech Head" image={tandon} />
         <Members name="Maitreyee" position="Creative Head" image={mait} />
-        <Members name="Ambar" position="Management Head" image={ambar} />
+        <Members name="Amogh" position="Mgmt Head" image={amogh} />
+        <Members name="Ambar" position="Mgmt Head" image={ambar} />
         <Members name="Karan" position="Volunteer" image={karan} />
         <Members name="Ankit" position="Volunteer" image={ankit} />
       </div>
